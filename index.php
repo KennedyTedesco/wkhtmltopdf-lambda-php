f12cdc5e6d6697cac5fae5dc74e9a238705d6b3b
Kennedy Tedesco <kennedyt.tw@gmail.com>
2019-07-27 16:27:48 -0300

    options

--- index.php
+++ index.php
@@ -10,11 +10,14 @@ lambda(static function (array $event) {
     $pdf = new Pdf('/opt/wkhtmltopdf');
 
     $options = [
+        'encoding' => 'utf-8',
         'page-size' => 'A4',
-        'margin-top'    => 0,
-        'margin-right'  => 0,
         'margin-bottom' => 0,
-        'margin-left'   => 0,
+        'margin-left' => 0,
+        'margin-top' => 0,
+        'margin-right' => 0,
+        'disable-smart-shrinking' => true,
+        'disable-javascript' => true,
     ];
 
     if (isset($event['options'])) {
